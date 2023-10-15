48993cdd6dedcec0a854e94ccfaa76a3ee6e9c0d
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-15 22:11:42 +0200

    add some func + json

---
+++ dist/concatenation.js
@@ -0,0 +1,4 @@
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
+export { concatenation };
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import { concatenation } from './concatenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (button && input) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello');
+    });
+}
